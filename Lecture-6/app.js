8065a13819cf550c4a344d2a93966cedcc2f7a29
Badal Chaudahry <99376974+badalsingh01@users.noreply.github.com>
2023-08-21 05:15:06 +0530

    Add files via upload

---
+++ Lecture-6/app.js
@@ -0,0 +1,101 @@
+// //var name = prompt('Enter your name')
+
+// //window.alert('Welcome to coding Blocks! '+name);
+// 'use strict'
+// console.log('Hello world!')
+
+// let x =10
+// console.log(x)
+// //x ='string'// or "string // 'A'
+// console.log(x)
+
+// let y = 100
+// console.log(y)
+
+// y = "Some String"
+// console.log(y)
+
+
+// let arr = ["badal", true, 10, 11.11, 123456.345]
+
+// console.log(arr[3])
+// console.log('Printing the array : ')
+// for (var i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+    // if(x>5){
+    //     var m = 10
+    //     console.log(y)
+    // }
+
+    // else{
+    //     console.log(arr)
+    // }
+
+    // console.log(m)
+
+    // // data types in js
+    // // string
+    // let  c = 'Name'
+    // console.log(typeof c)
+    // // number
+    // c = 1
+    // console.log(typeof c)
+    // //undefined
+    // c = undefined
+    // console.log(typeof c)
+    // // object
+    // c = ["badal",true,10,11.11,123456.345]
+    // console.log(typeof c)
+    // let obj = {
+    //     Kartik: "Mathur",
+    //     Badal: 'Chaudhary',
+    //     Abhishek: "Rao",
+    //     1: 'one',
+    //     ' ': true,
+    //     "Badal Chaudhary": "IT"
+    // }
+    // console.log(typeof obj)
+    // console.log(typeof true)
+
+// c = null
+// console.log(typeof c)
+
+
+// let a = 10 + '1'
+// console.log(a)
+
+// a = 10 + 1 + '1'
+// console.log(a)
+
+// let b = "ABC" - 1
+// console.log(b) 
+
+// console.log(1 == 1) // true
+// console.log(1 == '1') // true
+// console.log(1 === '1') // false
+
+// console.log(' ' == 0) // true
+
+// let arr = [1,2,3]
+// b = arr
+
+// console.log(arr == b)
+
+console.log(' ' == 0) // true
+console.log([] == 0) // true
+console.log(' ' == []) // false
+
+console.log(false == 0) // true
+console.log(true == 1) // true
+console.log(false == ' ') // true
+
+obj = {
+    a:10
+}
+
+obj.b = 20
+
+obj.obj=obj
+console.log([] + {}) // [object Object]
